9447332e90e1f35bb83e720c0d721649f27a98db
18194222770 <425274967@qq.com>
2022-11-29 17:33:31 +0800

    7 files modified

--- jackson/src/app/core/core.module.ts
+++ jackson/src/app/core/core.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
@@ -10,8 +10,9 @@ import { HeaderComponent } from './header/header.component';
   exports: [FooterComponent, SidebarComponent, HeaderComponent],
 })
 export class CoreModule {
-  
-  constructor{
-
+  constructor(@Optional() @SkipSelf() parent: CoreModule) {
+    if (parent) {
+      throw new Error('The CoreModule has already been loaded in pargram!');
+    }
   }
 }
